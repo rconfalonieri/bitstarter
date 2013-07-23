9f0e9fa9197326e57f52a2762cb03907ade69f60
Roberto Confalonieri <roberto.confalonieri@gmail.com>
2013-07-23 16:05:45 +0000

    Added index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
+var content = fs.readFileSync('index.html');
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  
+  //response.send('Hello World2!');
+  response.send(Buffer.toString(content));
+
 });
 
 var port = process.env.PORT || 5000;
